30035ceca97def7ec5ea729e423548a5170a3603
Marco-Vigilio <tonyvigilio@gmail.com>
2023-05-22 11:13:55 +0200

    altri dettagli in console.log

--- snack3/script/myscript.js
+++ snack3/script/myscript.js
@@ -4,27 +4,26 @@ Calcola la somma dei primi 10 numeri
 
 
 let somma = 0;
-
+/*
 let giri = 0;
 
 while(giri != 10){
     let num = parseInt(prompt("Inserisci il tuo " + (giri+1) + "° numero"));
     console.log("Il tuo " + (giri+1) + "° numero inserito: " + num);
     somma = somma + num;
-    console.log()
     giri = giri + 1;
 }
-
+console.log();
 console.log("La somma dei 10 numeri : " + somma);
 console.log("La media dei 10 numeri è : " + (somma / 10));
 
+*/
 
-/*
 for(let i = 0; i <10; i++){
     let num = parseInt(prompt("Inserisci il tuo " + (i+1) + "° numero"));
-    console.log("Numero inserito: " + num);
+    console.log("Il tuo " + (i+1) + "° numero inserito: " + num);
     somma = somma + num;
 }
+console.log();
 console.log("La somma dei 10 numeri : " + somma);
 console.log("La media dei 10 numeri: " + (somma / 10));
-*/
